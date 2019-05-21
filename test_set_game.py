b9bc48969d2a4ac602b5c6fb66d76b9597a63b8f
Mikhail Martynenko <michael@Mikhails-MacBook-Pro.local>
2019-05-21 22:35:17 +0300

    hz chto proishodit

---
+++ test_set_game.py
@@ -0,0 +1,49 @@
+import set_game, pytest
+
+card_set_true_1 = []
+card_set_true_1.append(set_game.Card(2,'DIAMOND','SOLID','PURPLE'))
+card_set_true_1.append(set_game.Card(2,'DIAMOND','SOLID','PURPLE'))
+card_set_true_1.append(set_game.Card(2,'DIAMOND','SOLID','PURPLE'))
+
+card_set_true_2 = []
+card_set_true_2.append(set_game.Card(1,'DIAMOND','SOLID','PURPLE'))
+card_set_true_2.append(set_game.Card(2,'DIAMOND','SOLID','PURPLE'))
+card_set_true_2.append(set_game.Card(3,'DIAMOND','SOLID','PURPLE'))
+
+card_set_true_3 = []
+card_set_true_3.append(set_game.Card(1,'DIAMOND','SOLID','PURPLE'))
+card_set_true_3.append(set_game.Card(2,'DIAMOND','STRIPPED','PURPLE'))
+card_set_true_3.append(set_game.Card(3,'DIAMOND','OPEN','PURPLE'))
+
+card_set_false_1 =[]
+card_set_false_1.append(set_game.Card(3, 'SQUIGGLE', 'SOLID', 'RED'))
+card_set_false_1.append(set_game.Card(2, 'OVAL', 'SOLID', 'GREEN'))
+card_set_false_1.append(set_game.Card(2, 'OVAL', 'OPEN', 'PURPLE'))
+
+card_set_false_2 =[]
+card_set_false_2.append(set_game.Card(3, 'OVAL', 'SOLID', 'GREEN'))
+card_set_false_2.append(set_game.Card(3, 'SQUIGGLE', 'STRIPPED', 'GREEN'))
+card_set_false_2.append(set_game.Card(2, 'SQUIGGLE', 'STRIPPED', 'PURPLE'))
+
+card_set_false_3 =[]
+card_set_false_3.append(set_game.Card(3, 'SQUIGGLE', 'STRIPPED', 'PURPLE'))
+card_set_false_3.append(set_game.Card(3, 'DIAMOND', 'OPEN', 'PURPLE'))
+card_set_false_3.append(set_game.Card(2, 'OVAL', 'SOLID', 'PURPLE'))
+
+
+def test_generate_set_len():
+    assert len(set_game.generate_set())==3
+
+def test_generate_set_card_type():
+    for card in set_game.generate_set():
+        assert isinstance(card,set_game.Card)
+
+def test_raise_error():
+    with pytest.raises(ValueError):
+        card = set_game.Card(4,"DIAMOND","STRIPPED","RED")
+
+def test_check_set_true():
+    assert set_game.check_set(card_set_true_1) and set_game.check_set(card_set_true_2) and set_game.check_set(card_set_true_3) == True
+
+def test_check_set_false():
+    assert not set_game.check_set(card_set_false_1) and not set_game.check_set(card_set_false_2) and not set_game.check_set(card_set_false_3) == True
---
+++ test_tictactoe.py
@@ -0,0 +1,60 @@
+import unittest
+import tictactoe
+
+board = [[2, 2, 0], [1, 0, 2], [1, 1, 0]] #все ок, игра идет
+board_x = [[1, 2, 2], [1, 0, 2], [1, 1, 0]] #все ок, выйграл Х
+board_false = [[2, 2, 0], [1, 0, 2], [1, 1, 0],[1, 1, 0]] #доска сломана
+board_0_x = [[2, 1, 0], [1, 2, 2], [1, 2, 0]] #ноликов больше - ошибка
+board_other = [[2, 2, 2], [1, 1, 1], [0, 0, 0]] #странная ситуация - выйграл и Х и 0
+
+class MyTictactoeTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print("[1,2,3,4,5 - начинаем проверять]")
+        print("v" * 30)
+    @classmethod
+    def tearDownClass(cls):
+        print("^" * 30)
+        print("[закончили проверять]")
+
+    def test_validate_board_type(self):
+        """ проверка типа bool"""
+        self.assertIsInstance(tictactoe.validate_board(board), bool)
+
+    def test_validate_board_result_true(self):
+        """проверка правильности результата True """
+        self.assertTrue(tictactoe.validate_board(board) is True)
+
+    def test_validate_board_result_false(self):
+        """проверка правильности результата False"""
+        self.assertFalse(tictactoe.validate_board(board_false) is True)
+
+    def test_validate_board_count_0_X(self):
+        """проверка числа «ноликов» и «крестиков»"""
+        self.assertFalse(tictactoe.validate_board(board_0_x) is True)
+
+    def test_game_finished(self):
+        """проверка  формат вывода (числовой)"""
+        self.assertIsInstance(tictactoe.game_finished(board), int)
+
+    def test_game_finished_still_going(self):
+        """проверка 0, если на поле ещё идёт игра"""
+        self.assertEqual(tictactoe.game_finished(board), 0)
+
+    def test_game_finished_someone_wins(self):
+        """проверка  1 или 2, если выиграл какой-то игрок"""
+        self.assertEqual(tictactoe.game_finished(board_x), 1 or 2)
+
+    def test_game_finished_something_else(self):
+        """проверка других случаев, где мы от функции ждём -1"""
+        self.assertEqual(tictactoe.game_finished(board_other), -1)
+
+    def test_render_board(self):
+        """проверка  формат вывода (str)"""
+        self.assertIsInstance(tictactoe.render_board(board), str)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
