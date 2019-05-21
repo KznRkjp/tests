cb2b4b875cbc645c55ddcf001b83c0026b9bc109
Mikhail Martynenko <michael@Mikhails-MacBook-Pro.local>
2019-05-21 23:34:14 +0300

    slight update

--- tests/test_tictactoe.py
+++ tests/test_tictactoe.py
@@ -1,5 +1,5 @@
 import unittest
-import tictactoe
+import tests/tictactoe
 
 board = [[2, 2, 0], [1, 0, 2], [1, 1, 0]] #все ок, игра идет
 board_x = [[1, 2, 2], [1, 0, 2], [1, 1, 0]] #все ок, выйграл Х
